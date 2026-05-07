3bf122ec7413e3ccce77576f4472059713f95eba
guanchulu <70261167+guanchulu@users.noreply.github.com>
2026-05-07 15:25:16 +0800

    Add English names for priority city delta layer

--- docs/interactive_map_public/static_site/expanded_chain_delta_patch.js
+++ docs/interactive_map_public/static_site/expanded_chain_delta_patch.js
@@ -1,6 +1,7 @@
 (function () {
   const DELTA_URL = "./data/expanded_chain_delta.json";
   const SUMMARY_URL = "./data/expanded_chain_summary.json";
+  const CITY_NAMES = {"110000":"Beijing","120000":"Tianjin","130100":"Shijiazhuang","140100":"Taiyuan","150100":"Hohhot","210100":"Shenyang","210200":"Dalian","220100":"Changchun","230100":"Harbin","310000":"Shanghai","320100":"Nanjing","320200":"Wuxi","320400":"Changzhou","320500":"Suzhou","320600":"Nantong","321000":"Yangzhou","321100":"Zhenjiang","321200":"Taizhou","330100":"Hangzhou","330200":"Ningbo","330300":"Wenzhou","330400":"Jiaxing","330600":"Shaoxing","340100":"Hefei","350100":"Fuzhou","350200":"Xiamen","350500":"Quanzhou","360100":"Nanchang","370100":"Jinan","370200":"Qingdao","410100":"Zhengzhou","420100":"Wuhan","430100":"Changsha","440100":"Guangzhou","440300":"Shenzhen","440400":"Zhuhai","440500":"Shantou","440600":"Foshan","440700":"Jiangmen","441300":"Huizhou","441900":"Dongguan","450100":"Nanning","450200":"Liuzhou","460100":"Haikou","500000":"Chongqing","510100":"Chengdu","510700":"Mianyang","520100":"Guiyang","530100":"Kunming","610100":"Xi'an","620100":"Lanzhou","650100":"Urumqi"};
 
   function statusLabel(status) {
     if (status === "model_result") return "Full pathway chain is available in the current optimized model.";
@@ -22,6 +23,10 @@
     props.tier2_priority_city = Boolean(delta[5]);
     props.dac_fallback_available = Boolean(delta[6]);
     props.screening_threshold_gross_mtco2 = 0.25;
+    if (CITY_NAMES[props.city_id]) {
+      props.city_name = CITY_NAMES[props.city_id];
+      props.city_name_en = CITY_NAMES[props.city_id];
+    }
     if (props.dac_fallback_available) {
       props.dac_cost_2060_usd_t = 285;
       props.dac_policy_credit_2060_usd_t = 180;
